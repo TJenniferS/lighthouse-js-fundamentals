829472b2e5be2f5922239ee6927b32993ea8fc29
TJenniferS <tanishqsharma20231@gmail.com>
2023-05-08 11:45:47 -0400

    randomchanges3

--- hello.js
+++ hello.js
@@ -1,9 +1,5 @@
-const sayHelloToConsole  = function (name) {
-  console.log("Hello, " + name);
-}
-sayHelloToConsole('John'); 
-
 const returnSayHello  = function (name) {
   return "Hello, " + name;
 }
 const greeting = returnSayHello('John');
+console.log(greeting);
